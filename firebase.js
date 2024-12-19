033b78375a5bf1aee4684a359e24a6614d967fd2
sjaypits <sjaypits@gmail.com>
2024-12-19 16:42:30 +0800

    Basic CRUD for s4's Product

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDG7jCmN_7BDZB0FJv8XlhQbrjfAXJ5Xfg",
+  authDomain: "ssss-a16e6.firebaseapp.com",
+  databaseURL: "https://ssss-a16e6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ssss-a16e6",
+  storageBucket: "ssss-a16e6.firebasestorage.app",
+  messagingSenderId: "350473624117",
+  appId: "1:350473624117:web:5216b63f7412a1b563af7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
